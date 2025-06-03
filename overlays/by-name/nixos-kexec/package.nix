19e41bf3f5496067178320355aff799e609f614e
Jared Baur <jaredbaur@fastmail.com>
2025-06-03 09:45:11 -0400

    Just use system-wide config for more stuff

---
+++ overlays/by-name/nixos-kexec/package.nix
@@ -0,0 +1,27 @@
+{
+  argc,
+  coreutils-full,
+  fzf,
+  jq,
+  kexec-tools,
+  lib,
+  writeShellApplication,
+}:
+
+writeShellApplication {
+  name = "nixos-kexec";
+
+  runtimeInputs = [
+    argc
+    coreutils-full
+    jq
+    fzf
+    kexec-tools
+  ];
+
+  text =
+    ''
+      kexec_jq=${./nixos-kexec.jq}
+    ''
+    + lib.fileContents ./nixos-kexec.bash;
+}
